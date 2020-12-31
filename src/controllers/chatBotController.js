b42f56e8ebd5415770b8a8f88cfba68027070c38
crisalder <lednguyenvu@gmail.com>
2020-12-31 12:29:24 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -45,7 +45,7 @@ const retrieveProfile = (psid) => {
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         // Print out the response body
-        console.log(reponse, body);
+        console.log(response, body);
       } else {
         console.log(123);
       }
