45c57aae188f338ea6b041d3a8d6d0a9aaa49587
crisalder <lednguyenvu@gmail.com>
2021-01-03 16:33:19 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const BASE_URL = process.env.BASE_URL;
-let firstName, lastName = null;
+let firstName, lastName, gender = null;
 
 const test = (req, res) => {
   var messageData = {
@@ -83,14 +83,15 @@ const postWebhook = (req, res) => {
       // pass the event tos the appropriate handler function
       if (webhook_event.message) {
         retrieveProfile(sender_psid).then(res => {
-          console.log(res.data, 111);
           lastName = res.data.last_name;
+          gender = res.data.gender === 'male' ? 'anh' : 'chị';
           handleMessage(sender_psid);
         });
       } else if (webhook_event.postback) {
         if (webhook_event.postback.payload === 'USER_DEFINED_PAYLOAD') {
           retrieveProfile(sender_psid).then(res => {
             lastName = res.data.last_name;
+            gender = res.data.gender === 'male' ? 'anh' : 'chị';
             handleMessage(sender_psid);
           });
         } else {
@@ -117,7 +118,7 @@ function handleMessage(sender_psid, received_message) {
       payload: {
         template_type: "button",
         text:
-          `Cảm ơn bạn ${lastName} đã nhắn tin cho bếp 3CE, bạn cần tìm hiểu thông tin nào dưới đây ạ?`,
+          `Cảm ơn ${gender} ${lastName} đã nhắn tin cho bếp 3CE, bạn cần tìm hiểu thông tin nào dưới đây ạ?`,
         buttons: [
           {
             type: "postback",
